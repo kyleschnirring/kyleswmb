50781b240dd8cb6d3120f8517c5ac162ef390b41
kyleschnirring <kyleschnirring@icloud.com>
2016-02-19 16:25:50 -0800

    adding info windows

--- droppingallthepins.js
+++ droppingallthepins.js
@@ -39,9 +39,9 @@ function dropAllThePins() {
     path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
     fillColor: 'yellow',
     fillOpacity: 0.8,
-    scale: 0.05,
+    scale: 0.09,
     strokeColor: 'gold',
-    strokeWeight: 14
+    strokeWeight: 1
   };
 
   var you = new google.maps.Marker({
@@ -50,18 +50,34 @@ function dropAllThePins() {
     map: map
   });
 
-  var marker;
+  var contentString = '<div class="content">'+
+      '<div class="busStop">'+
+      '</div>'+
+      '<h1 class="firstHeading"></h1>'+
+      '<div class="bodyContent">'+
+      '<p></p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  //var marker;
 
   for (var i = 0; i < busStopObjects.length; i++) {
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
       position: new google.maps.LatLng(busStopObjects[i].lat, busStopObjects[i].lon),
       map: map
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
     });
   }
 }
 
 google.maps.event.addDomListener(window, 'load', dropAllThePins);
-
+/*
 function initMap() {
   if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -84,7 +100,7 @@ function initMap() {
   });
 // To add the marker to the map, call setMap();
 marker.setMap(map);
-}
+}*/
 
 //initMap();
 //dropAllThePins();
